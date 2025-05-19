d1e5e253c950a0ea20c28d0486c5767fb64d06a2
Karina Chernobai <karina.chernobai@gmail.com>
2025-05-19 15:43:52 +0200

    Add func declarations

--- js/types.js
+++ js/types.js
@@ -1,14 +1,18 @@
-  /**
-   * Create a JsonData object.
-   * @param {number} year - The year of the report.
-   * @param {Page[]} pages - An array of Page instances.
-   */
+export { JsonData, Page };
+
+/**
+ * Create a JsonData object.
+ * @param {number} year - The year of the report.
+ * @param {Page[]} pages - An array of Page instances.
+ * @throws {Error} Throws an error if year is not provided.
+ * @throws {Error} Throws an error if pages are not provided.
+ */
 class JsonData {
     constructor(year, pages) {
         // validation
     }
 
-    displayHTML(container){
+    display(container) {
         // displays the content on the page
     }
 }
@@ -19,13 +23,15 @@ class JsonData {
      * @param {string[]} images - An array of image URLs (at least one is required).
      * @param {string} paragraph - An optional paragraph of text.
      * @param {string[]} highlights - An optional array of highlights.
+     * @throws {Error} Throws an error if title is not provided.
+     * @throws {Error} Throws an error if images are not provided.
  */
 class Page {
     constructor(title, images, paragraph = "", highlights = []) {
         // validation
     }
 
-    displayHTML(container){
+    display(container) {
         // displays the page
     }
 }

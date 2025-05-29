f16db0f280f2c87568fc372fc6a1bb9192bd80ae
Karina Chernobai <karina.chernobai@gmail.com>
2025-05-29 19:29:28 +0200

    Bug fix

--- js/config.js
+++ js/config.js
@@ -1,9 +1,11 @@
 import { buildReportDom }  from './domBuilder.js';
 
 /* ----------------  CHANGE PATHS ONCE HERE  ---------------- */
-export const JSON_ROOT  = '../data/reports/';     // Folder for *.json reports
+const repoFolder = '/' + window.location.pathname.split('/')[1];
+const BASE_PATH = window.location.hostname !== 'localhost' ? repoFolder : '';
+export const JSON_ROOT  = BASE_PATH + '/data/reports/';     // Folder for *.json reports
 
-export const MEDIA_ROOT = '../';          // Prefix for every img / bg
+export const MEDIA_ROOT = BASE_PATH + '/';          // Prefix for every img / bg
 
 export const TARGET_TAG = 'main';         // Tag where to insert render
 

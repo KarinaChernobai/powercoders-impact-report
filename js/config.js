5db0d12b640e3d15e3c3df3d0f6ec041036ce234
Karina Chernobai <karina.chernobai@gmail.com>
2025-05-29 19:38:55 +0200

    Fix bug

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ import { buildReportDom }  from './domBuilder.js';
 /* ----------------  CHANGE PATHS ONCE HERE  ---------------- */
 const BASE_PATH = window.location.origin + (window.location.pathname.includes('powercoders-impact-report') ? '/powercoders-impact-report' : '');
 export const JSON_ROOT  = BASE_PATH + '/data/reports/';     // Folder for *.json reports
-export const MEDIA_ROOT = '../';          // Prefix for every img / bg
+export const MEDIA_ROOT = BASE_PATH + '/';          // Prefix for every img / bg
 
 export const TARGET_TAG = 'main';         // Tag where to insert render
 
